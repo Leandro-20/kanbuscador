b5c85399211b4f894ccdb2876bb639b12947f07b
Leandro Tevez <leandront20@gmail.com>
2022-03-17 17:48:19 -0300

    vercion 1.1.0

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
 const axios = require('axios').default
-const buscador = async ({url, filterProperty, text, layerName, title}) => {
-     const result = await axios({
+
+const getGeonodeResult = ({url, filterProperty, text, layerName}) => {
+    return axios({
         url: `${url}`,
         data: `
-        <wfs:GetFeature service="WFS" version="1.1.0" xmlns:gml="http://www.opengis.net/gml" xmlns:wfs="http://www.opengis.net/wfs" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" startIndex="0" maxFeatures="20">
-                <wfs:Query typeName="${layerName}" srsName="EPSG:4326">
-                    <wfs:SortBy>
-                        <wfs:SortProperty>
-                            <ogc:PropertyName>numero_obr</ogc:PropertyName>
-                            <wfs:SortOrder>A</wfs:SortOrder>
-                        </wfs:SortProperty>
-                    </wfs:SortBy>
-                    <ogc:Filter>
+    <wfs:GetFeature service="WFS" version="1.1.0" xmlns:gml="http://www.opengis.net/gml" xmlns:wfs="http://www.opengis.net/wfs" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" startIndex="0" maxFeatures="20">
+            <wfs:Query typeName="${layerName}" srsName="EPSG:4326">
+                <wfs:SortBy>
+                    <wfs:SortProperty>
+                        <ogc:PropertyName>numero_obr</ogc:PropertyName>
+                        <wfs:SortOrder>A</wfs:SortOrder>
+                    </wfs:SortProperty>
+                </wfs:SortBy>
+                <ogc:Filter>
+                    <ogc:And>
                         <ogc:And>
                             <ogc:And>
-                                <ogc:And>
-                                    <ogc:PropertyIsLike matchCase="false" wildCard="*" singleChar="." escapeChar="u0021">
-                                        <ogc:PropertyName>${filterProperty}</ogc:PropertyName>
-                                        <ogc:Literal>*${text}*</ogc:Literal>
-                                    </ogc:PropertyIsLike>
-                                </ogc:And>
+                                <ogc:PropertyIsLike matchCase="false" wildCard="*" singleChar="." escapeChar="u0021">
+                                    <ogc:PropertyName>${filterProperty}</ogc:PropertyName>
+                                    <ogc:Literal>*${text}*</ogc:Literal>
+                                </ogc:PropertyIsLike>
                             </ogc:And>
                         </ogc:And>
-                    </ogc:Filter>
-                </wfs:Query>
-            </wfs:GetFeature>
-       `,
+                    </ogc:And>
+                </ogc:Filter>
+            </wfs:Query>
+        </wfs:GetFeature>
+   `,
         headers: {
-        'Content-Type': 'application/xml'
+            'Content-Type': 'application/xml'
         },
         method: 'POST'
     })
+}
+const getNominatimResult = ({text}) => {
+    return axios({
+    url:`https://nominatim.openstreetmap.org/search?q=${text}&countrycodes=ar&format=geojson`
+})
+}
 
+const buscador = async ({url, filterProperty, text, layerName, title, type= 'geonode'}) => {
+    let result 
+    switch (type) {
+        case 'nominatim':
+          result = await getNominatimResult({text})
+          break;
+        case 'geonode':
+            result = await getGeonodeResult({url, filterProperty, text, layerName})
+      }
     return {title, result:result.data.features}
 }
   exports.buscador = buscador
@@ -45,5 +61,5 @@ exports.multiBuscador = multiBuscador
 // multiBuscador([{url:'https://visor.obraspublicas.gob.ar/gs/ows?service=WFS&outputFormat=json',filterProperty:'id_obra', text: '1', title:'hola',layerName: 'geonode:obras_localizacion_puntos' },
 // {url:'https://visor.obraspublicas.gob.ar/gs/ows?service=WFS&outputFormat=json',filterProperty:'id_obra', text: 'c', title:'hola',layerName: 'geonode:obras_localizacion_puntos' }])
 
-// buscador({url:'https://visor.obraspublicas.gob.ar/gs/ows?service=WFS&outputFormat=json',filterProperty:'id_obra', text: '1', title:'hola',layerName: 'geonode:obras_localizacion_puntos0' })
-// .then(({ title, result}) => {console.log({title, result})})
+//  buscador({url:'https://visor.obraspublicas.gob.ar/gs/ows?service=WFS&outputFormat=json',filterProperty:'id_obra', text: 'obelisco', title:'hola',layerName: 'geonode:obras_localizacion_puntos0' })
+//  .then(({ title, result}) => {console.log({title,result})})
